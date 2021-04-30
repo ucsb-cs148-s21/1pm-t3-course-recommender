ff28a0ec59947334c9987e99d102987826e89726
Alisha Zhang <alishaz@alishas-mbp.lan>
2021-04-30 13:03:08 -0700

    update navbar

---
+++ frontend_new/src/components/nav-bar/elements.js
@@ -0,0 +1,50 @@
+import { NavLink as Link } from 'react-router-dom';
+import styled from 'styled-components';
+
+export const Nav = styled.nav`
+  background: #f4bc1c;
+  height: 80px;
+  display: flex;
+  justify-content: space-between;
+  padding: 10px 500px;
+`;
+
+export const NavLink = styled(Link)`
+  color: #203354;
+  display: flex;
+  align-items: center;
+  text-decoration: none;
+  padding: 1rem 2rem;
+  height: 100%;
+  // cursor: pointer;
+  font-size: 18px;
+
+  &.active {
+    font-weight: 1000;
+  }
+`;
+
+export const NavBtn = styled.nav`
+  display: flex;
+  align-items: center;
+  margin-right: 20px;
+
+  // @media screen and (max-width: 768px) {
+  //   display: none;
+  }
+`;
+
+export const NavBtnLink = styled(Link)`
+  border-radius: 5px;
+  background: #203354;
+  padding: 10px 20px;
+  color: #fff;
+  // cursor: pointer;
+  // transition: all 0.2s ease-in-out;
+  // text-decoration: none;
+
+  &:hover {
+    background: #fff;
+    color: #000;
+  }
+`;
