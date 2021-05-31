020d03b96761883a06b56f8abfa63aa364c9e084
Justin Surmani <justinsurmani@ucsb.edu>
2021-05-31 13:51:13 -0700

    js - added profile page

--- frontend_new/src/App.js
+++ frontend_new/src/App.js
@@ -4,12 +4,14 @@ import NavBar from './components/nav-bar';
 import CourseComponent from './components/CourseComponent';
 import { Result } from './components/Result';
 import { CourseForm } from './components/CourseForm';
+import  Profile from './components/Profile'
 import {
   BrowserRouter as Router,
   Switch,
   Route,
 } from "react-router-dom";
 import LandingPage from './components/LandingPage';
+import PrivateRoute from './components/nav-bar/PrivateRoute'
 
 function App() {
   return (
@@ -20,6 +22,7 @@ function App() {
         <Route path='/Survey' component={CourseForm} />
         <Route path='/Input' component={CourseComponent} />
         <Route path='/Result' component={Result} />
+        <PrivateRoute path="/Profile" component={Profile} />
         {/* <Route path='/Home' component={LandingPage} /> */}
       </Switch>
     </Router>
